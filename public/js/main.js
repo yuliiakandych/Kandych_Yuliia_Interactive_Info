1e10b5bf8e14a23c4844367f9394d40227e82336
Yuliia Kandych <yuliiakandych@Yuliia-DIPLY-0138-24804.local>
2019-12-02 02:50:02 -0500

    all files

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,81 @@
-(()=>{
-    let thumbnails = document.querySelectorAll(".plantPictures");
+const myVM = (() => {
+  // get the user buttons and fire off an async DB query with Fetch
+  let userButtons = document.querySelectorAll( ' .c-link'),
+  lightbox = document.querySelector('.lightbox');
+
+
+
+
+
+  function parseCountryData(country) {
+
+    let targetDiv = document.querySelector(' .lb-content');
+  
+
+     let countryDescription = `
+     <h3 class="number">${country.ID}</h3>
+  <h4 class="country-desc"> <span>${country.description}<span></h4>
+  <p class="desc-js">${country.country}</p>
+  <img class="cover-cover" src="images/${country.cover}">
+  
+ `;
     
-    function parseData(plant){
-        let bio = document.querySelector('.bio')
-        bio.classList.add('bio_display')
-        bio.innerHTML = `<h3>${plant.Name}</h3>`;
-        bio.innerHTML += `<h4>${plant.Growing_type}</h4>`;
-    }
-    
-    function gettingData(e){
-        e.preventDefault();
-        console.log("clicked");
-        let url = `/users/${this.getAttribute('href')}`
-        console.log(url)
-    
-        fetch(url)
-        .then(res => {
-            
-          return  res.json()
-            
-        })
+  
+
+  console.log(countryDescription);
+  targetDiv.innerHTML = countryDescription;
+  lightbox.classList.add('show-lb');
+  
+
+  }
+
+
+  // function parseCountryData(country) {// person in the database is a result//model, lightbox, loading animation, css, js magic
+  //     let targetDiv = document.querySelector(' .lb-content'),
+  //         targetImg = lightbox.querySelector('.imgs');
+
+
+  //     let CountryDescription = `
+  //     <p class="desc-js">${country.description}</p>
+  //     <h4 class="quote-js"> Book quote:</h4>
+  //   `;
+
+  //     // console.log(CountryDescription);
+  //     // targetDiv.innerHTML = CountryDescription;
+  //     targetImg.src = country.imgsrc;
+
+  //     lightbox.classList.add('show-lb');
+
+
+  // }
+
+  function getCountryData(event) {
+      event.preventDefault();// kill the default a tag behaviour (dont navigate anywhere)
+      //debugger;
+
+      // find the image closet to the anchor tag and get its src property
+
+      let imgSrc = this.previousElementSibling.getAttribute('src');
+       let users = `/users/${this.getAttribute('href')}`;//  /3 route in express. passing data dynamically
+
+      fetch(users)// go get the data fetch boy!
+        .then(res => res.json())// parse the json result into a plain object
         .then(data => {
-           console.log(data)
-           parseData(data)})
-        
-    
-    }
-    
-    thumbnails.forEach(thumbnail => thumbnail.addEventListener('click',gettingData))
-    
-    
-    })();
+            console.log("my data resutl is: ", data);
+
+            data[0].imgsrc = imgSrc;
+
+
+            parseCountryData(data[0]);
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+  }
+
+  userButtons.forEach(button => button.addEventListener('click', getCountryData));
+  lightbox.querySelector(' .close').addEventListener('click',  function() {
+      lightbox.classList.remove('show-lb');
+  });
+
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,61 @@
 const express = require('express');
 const router = express.Router();
 
-const sql = require('../utils/sql')
+const sql = require('../utils/sql');
 
-router.get('/', (req,res)=>{
-    console.log("at the main route");
-   let query = "Select * from tbl_morewine";
-   sql.query(query, (err,result)=>{
-       console.log("getting data")
-       if (err){
-           throw err;
-           console.log(err);
-       }
-       console.log("My data is beautiful"+result);
-       res.render('index', {plants: result})
-   })
-})
 
-router.get('/users/:id', (req,res) =>{
-    console.log("popingup");
-    console.log(req.params.id)
-    let query = `Select * from tbl_morewine where ID=${req.params.id} `;
-   sql.query(query, (err,result)=>{
-       console.log("getting data")
-       if (err){
-           throw err;
-           console.log(err);
-       }
-       console.log(result[0])
-       res.json(result[0])
+
+
+
+router.get('/', (req, res) => {
+    // should really get the user data here and then fetch it thru, but let's try this asynchronously
+    console.log('at the main route');
+
+    let query = "SELECT  ID, country, img FROM tbl_morewine";
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        console.log(result); // should see objects wrapped in an array
+
+        // render the home view with dynamic data
+        res.render('index', { data: result });
+     
+        
+
        
-
+    })
 })
-})
+//looling for localhost:3000/anything
+router.get('/users/:id', (req, res) => {
+    console.log("hit a dynamic route");
+    console.log(req.params.id);
 
+
+    let query = `SELECT  * FROM tbl_morewine WHERE ID="${req.params.id}"`;
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        console.log(result); // should see objects wrapped in an array
+        // turn our social property into an array - its just text in the DB
+        // wich isnt really anything we can work with
+
+    //      result[0].quote = result[0].quote.split(',').map(function(item) {
+    //      item = item.trim(); //remove the extra spaces from each word
+
+    //      return item;
+    //       });
+    //    console.log('after split: ', result[0]);
+      
+
+
+        // render the home view with dynamic data
+        //res.render('home', { data: result });
+        res.json(result);
+
+       
+    })
+})
 
 module.exports = router;
